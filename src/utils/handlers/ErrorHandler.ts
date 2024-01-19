5c26bfa8b7ed7dba492f14f25025dab869fed544
Dinesh Kumar A <dineshkumargits@gmail.com>
2024-01-19 18:42:41 +0530

    [Dinesh] modifies in ErrorHandler

--- src/utils/handlers/ErrorHandler.ts
+++ src/utils/handlers/ErrorHandler.ts
@@ -1,12 +1,12 @@
-import { Response } from "express";
+import { Response } from 'express';
 export default class ErrorHandler {
   public static response(res: Response, code: number, message: string, data: Record<string, any> | [] | null) {
-    const retrn = {
-      status: "ERROR",
+    const response = {
+      status: 'ERROR',
       message,
       data,
     };
-    res.status(code).send(retrn);
-    return retrn;
+    res.status(code).send(response);
+    return response;
   }
 }
