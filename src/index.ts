960f1bde0db7fcbd83584c1c4134b12d4d52574f
Dinesh Kumar A <dineshkumargits@gmail.com>
2024-01-19 07:12:32 +0530

    [Dinesh] changes in index.ts for testing server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('Is this working?');
+import express, { Application, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+const app: Application = express();
+
+app.get('/', async (req: Request, res: Response): Promise<Response> => {
+  return res.status(200).send({
+    message: 'Hello World!',
+  });
+});
+
+const PORT = 3000;
+
+try {
+  app.listen(PORT, (): void => {
+    console.log(`Connected successfully on port ${PORT}`);
+  });
+} catch (error: any) {
+  console.error(`Error occured: ${error.message}`);
+}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post('/post', async (req: Request, res: Response): Promise<Response> => {
+  console.log(req.body);
+  return res.status(200).send({
+    message: 'Hello World from post!',
+  });
+});
