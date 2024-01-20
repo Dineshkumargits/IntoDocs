d43bd1676a2ad7f0af3c588e1148f79aa6146b81
Dinesh Kumar A <dineshkumargits@gmail.com>
2024-01-21 00:37:30 +0530

    [Dinesh] adds documents related routes

---
+++ src/documents/documents.ts
@@ -0,0 +1,128 @@
+import express from 'express';
+import { Op } from 'sequelize';
+import DocumentPermission from '../../models/DocumentPermissions';
+import Document from '../../models/Documents';
+import User from '../../models/User';
+import { validateAdmin, validateUser } from '../middleware/validateUser';
+import { ResponseHandler } from '../utils/handlers';
+const router: express.Router = express.Router();
+router.get('/list_doc_box/:parent_document_id', validateUser, async (req, res) => {
+  const user = (req as any).user;
+  const { parent_document_id } = req.params as any;
+  let documents = [];
+  if (user.user_role_id == 1) {
+    documents = await Document.findAll({
+      where: {
+        ...(Number(parent_document_id) != 0
+          ? {
+              parent_document: Number(parent_document_id),
+            }
+          : {}),
+      },
+      include: [{ model: DocumentPermission, include: [{ model: User, attributes: { exclude: ['password'] } }] }],
+    });
+  } else {
+    const permissions = await DocumentPermission.findAll({
+      where: {
+        user_id: user?.user_id,
+      },
+    });
+    documents = await Document.findAll({
+      where: {
+        parent_document: parent_document_id,
+        document_id: { [Op.in]: permissions?.map((s) => s.document_id) },
+      },
+      raw: true,
+    });
+  }
+  res.json(documents);
+});
+router.post('/create_doc_box', validateAdmin, async (req, res) => {
+  const user = (req as any).user;
+  const { user_ids } = (req as any).body;
+  const last_document = await Document.findOne({
+    where: {
+      is_doc_box: true,
+    },
+    order: [['document_id', 'DESC']],
+    limit: 1,
+  });
+  const created_document = await Document.create({
+    name: `DocBox-${String((last_document?.document_id || 0) + 1).padStart(5, '0')}`,
+    is_doc_box: true,
+  });
+  for (const user_id of user_ids) {
+    await DocumentPermission.create({
+      document_id: created_document.document_id,
+      user_id,
+    });
+  }
+  ResponseHandler.response(res, 200, 'success', created_document);
+});
+
+router.post('/update_doc_box', validateAdmin, async (req, res) => {
+  const { user_ids, document_id } = (req as any).body;
+  await DocumentPermission.destroy({
+    where: {
+      document_id,
+    },
+  });
+  for (const user_id of user_ids) {
+    await DocumentPermission.create({
+      document_id: document_id,
+      user_id,
+    });
+  }
+  res.json('Updated');
+});
+
+router.delete('/delete_doc_box/:document_id', validateAdmin, async (req, res) => {
+  const { document_id } = req.params as any;
+  await DocumentPermission.destroy({
+    where: {
+      document_id,
+    },
+  });
+  await Document.destroy({
+    where: {
+      document_id,
+    },
+  });
+  res.json('Deleted');
+});
+
+router.post('/upload_documents', validateUser, async (req, res) => {
+  const user = (req as any).user;
+  const { files } = (req as any).body;
+  for (const file of files) {
+    await Document.create({
+      ...file,
+      uploaded_by: user?.user_id,
+    });
+  }
+  res.json('Document created');
+});
+
+router.get('/list_documents/:docbox_id', validateUser, async (req, res) => {
+  const user = (req as any).user;
+  const { docbox_id } = req.params as any;
+  let documents = [];
+  if (user?.user_role_id == 1) {
+    documents = await Document.findAll({
+      where: {
+        parent_document: Number(docbox_id),
+      },
+      include: [{ model: DocumentPermission, include: [{ model: User, attributes: { exclude: ['password'] } }] }],
+    });
+  } else {
+    documents = await Document.findAll({
+      where: {
+        parent_document: Number(docbox_id),
+        uploaded_by: user?.user_id,
+      },
+    });
+  }
+  res.json(documents);
+});
+
+module.exports = router;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import multer from 'multer';
 import { ResponseHandler } from './utils/handlers';
+import { validateAdmin } from './middleware/validateUser';
+import User from '../models/User';
+import { Op } from 'sequelize';
 
 export const uploadinMem = multer({
   storage: multer.memoryStorage(),
@@ -19,5 +22,16 @@ router.get('/hello', (req, res) => {
   ResponseHandler.response(res, 200, 'Ok for Hello', null);
 });
 router.use('/auth', require('./auth/index'));
+router.use('/documents', require('./documents/documents'));
+router.use('/file', require('./fileupload/upload').default);
+router.get('/get_all_clients', validateAdmin, async (req, res) => {
+  const clients = await User.findAll({
+    attributes: { exclude: ['password'] },
+    where: {
+      user_role_id: { [Op.ne]: 1 },
+    },
+  });
+  return ResponseHandler.response(res, 200, 'success', clients);
+});
 
 module.exports = router;
