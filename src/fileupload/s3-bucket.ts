32d075455f484c3a65525ab2e1e4cee03b52a370
Dinesh Kumar A <dineshkumargits@gmail.com>
2024-01-21 00:36:47 +0530

    [Dinesh] configures s3 bucket

---
+++ src/fileupload/s3-bucket.ts
@@ -0,0 +1,20 @@
+const S3_ID = process.env.S3_ID || '';
+const S3_SECRET = process.env.S3_SECRET || '';
+const BUCKET_NAME = process.env.BUCKET_NAME || '';
+const FILE_EXPIRATION_TIME = process.env.FILE_EXPIRATION_TIME || '60';
+const S3_REGION = process.env.S3_REGION || 'ap-south-1';
+const S3_SIGNATURE = process.env.S3_SIGNATURE || 'v4';
+const DEFAULT_MAX_FILESIZE = 5;
+const BYTES_TO_MB_CONVERSION = 1024 * 1024;
+const MAX_UPLOAD_FILESIZE = DEFAULT_MAX_FILESIZE * BYTES_TO_MB_CONVERSION; // 5MB
+
+export {
+  BUCKET_NAME,
+  S3_ID,
+  S3_SECRET,
+  FILE_EXPIRATION_TIME,
+  S3_REGION,
+  S3_SIGNATURE,
+  DEFAULT_MAX_FILESIZE,
+  MAX_UPLOAD_FILESIZE,
+};
