d9dad231311f63f70082b77dec13d33eb2afaf8c
Dinesh Kumar A <dineshkumargits@gmail.com>
2024-01-21 00:33:32 +0530

    [Dinesh] adds migration to create documents table

---
+++ migrations/20240119221419-create_documents_table.ts
@@ -0,0 +1,63 @@
+import { QueryInterface, DataTypes } from 'sequelize';
+const TABLE_NAME = 'documents';
+
+export function up(queryInterface: QueryInterface): Promise<void> {
+  return queryInterface.createTable(TABLE_NAME, {
+    document_id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      allowNull: true,
+      type: DataTypes.STRING,
+    },
+    s3_url: {
+      allowNull: true,
+      type: DataTypes.STRING,
+    },
+    is_doc_box: {
+      allowNull: true,
+      type: DataTypes.BOOLEAN,
+    },
+    parent_document: {
+      allowNull: true,
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'documents',
+        key: 'document_id',
+      },
+    },
+    uploaded_by: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'users',
+        key: 'user_id',
+      },
+    },
+    size: {
+      type: DataTypes.INTEGER,
+    },
+    type: {
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    deletedAt: {
+      allowNull: true,
+      type: DataTypes.DATE,
+    },
+  });
+}
+
+export function down(queryInterface: QueryInterface): Promise<void> {
+  return queryInterface.dropTable(TABLE_NAME);
+}
